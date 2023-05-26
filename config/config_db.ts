f4378ac7ae2fd0ac6f1f676ccd67ec234e5685ea
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-27 01:09:06 +0500

    database configurated

---
+++ config/config_db.ts
@@ -0,0 +1,10 @@
+export const config = {
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432,
+    password: 'j24xt200',
+    database: 'nest',
+  },
+};
