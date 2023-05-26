c3c9f424742d5d61c7ebcb5d6ea509905aece6b6
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-27 01:14:07 +0500

    Quizs Module imported in main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { KnexConfigModule } from './knex-config/knex-config.module';
+import { QuizsModule } from './quizs/quizs.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, KnexConfigModule, QuizsModule],
   controllers: [AppController],
   providers: [AppService],
 })
